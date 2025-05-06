72514fe8e59e50d53d28ca459a68ae4f4f6deeee
hirashahbaz92@gmail.com <hirashahbaz92@gmail.com>
2025-05-06 09:58:12 +0000

    updated query

--- models/my_first_model_demo.sql
+++ models/my_first_model_demo.sql
@@ -1,7 +1,15 @@
-{{ config(materialized='view') }}
+{{ config(
+    materialized='table'
+) }}
 with source_data as (
-select  * from dbc.tablesv
-where Databasename='SYSUDTLIB'
+SELECT
+    CustomerID,
+    COUNT(*) AS order_count,
+    SUM(Amount) AS total_spent,
+    MIN(OrderDate) AS first_order_date,
+    MAX(OrderDate) AS last_order_date
+FROM SalesDB.Orders
+GROUP BY CustomerID
 )
 
 select * from source_data
